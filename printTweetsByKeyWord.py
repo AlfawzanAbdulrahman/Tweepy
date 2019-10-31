ca4fe803c32870eb7555f0b26186d7dac003288b
Abdulrahman Alfawzan <54145302+AlfawzanAbdulrahman@users.noreply.github.com>
2019-11-01 00:21:25 +0300

    Create printTweetsByKeyWord.py

---
+++ printTweetsByKeyWord.py
@@ -0,0 +1,27 @@
+import tweepy
+
+# Authenticate Your Credentials
+
+#
+auth = tweepy.OAuthHandler("API key", "API secret key")
+
+auth.set_access_token("Access token", "Access token secret")
+
+api = tweepy.API(auth)
+
+# Method: Print Tweets by a keyword in a specific language and specific
+
+
+try:
+	numOfTwwets = 10
+	file = open("tweets.txt", "w+")
+	for tweet in api.search(q="Saudi Arabia", lang="en", rpp=numOfTwwets):
+		file.write(f"{tweet.user.name}:{tweet.text}")
+		file.write(4*"\n")
+
+except IOError:
+	file = open(file, "w+")
+file.close()
+
+
+print("{} tweets have been printed in the file named {} ".format(numOfTwwets, file.name))
